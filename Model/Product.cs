037ed813cb7367832d1ac3e198271cfd55238c7d
PriyaGhorude3 <93178917+PriyaGhorude3@users.noreply.github.com>
2021-12-01 15:21:38 +0530

    First commit

---
+++ Model/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerPractice.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+    }
+}
